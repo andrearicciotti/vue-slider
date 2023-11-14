49c81e87fc66979cf8dbc88edc82b59fce1d4638
andrearicciotti <ricciotti.andrea1993@gmail.com>
2023-11-14 17:07:55 +0100

    add: image slider with thumbs interactive image

--- README.md
+++ README.md
@@ -7,9 +7,9 @@
 1. Partendo dal markup della versione svolta in js plain, rifare lo slider ma questa volta usando Vue.
 
 2. Bonus
-- al click su una thumb, visualizzare in grande l'immagine corrispondente
-- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
-- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
+- al click su una thumb, visualizzare in grande l'immagine corrispondente.
+- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente.
+- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce.
 
 ---
 
--- js/script.js
+++ js/script.js
@@ -3,11 +3,45 @@ const { createApp } = Vue;
 const app = createApp({
     data() {
         return {
-
-    };
+            images: [
+                {
+                    imagePath: "./img/01.webp",
+                    title: "Marvel's Spiderman Miles Morales",
+                    caption: "Spiderman dolor sit amet, consectetur adipisicing elit. Dicta iusto"
+                },
+                {
+                    imagePath: "./img/02.webp",
+                    title: "Ratchet and Clank 4 is coming",
+                    caption: "Ratchet and Clank dolor sit amet, consectetur adipisicing elit. Dicta iusto."
+                },
+                {
+                    imagePath: "./img/03.webp",
+                    title: "Fortnite new season",
+                    caption: "Fortnite dolor sit amet, consectetur adipisicing elit. Dicta iusto."
+                },
+                {
+                    imagePath: "./img/04.webp",
+                    title: "Stray",
+                    caption: "The cat dolor sit amet, consectetur adipisicing elit. Dicta iusto."
+                },
+                {
+                    imagePath: "./img/05.webp",
+                    title: "Avengers endendgame?",
+                    caption: "Thanos strikes back dolor sit amet, consectetur adipisicing elit. Dicta iusto."
+                },
+            ],
+            showClass: "active",
+            activeIndex: 0
+        };
     },
     methods: {
+        nextSlide() {
+            (this.activeIndex === this.images.length - 1) ? this.activeIndex = 0 : this.activeIndex++;
+        },
 
+        prevSlide() {
+            (this.activeIndex === 0) ? this.activeIndex = this.images.length - 1 : this.activeIndex--;
+        }
     }
 })
 
